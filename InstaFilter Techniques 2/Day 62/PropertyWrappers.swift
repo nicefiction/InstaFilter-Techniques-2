467edc492511eb95c99c4c596ada6e82a31bb868
Olivier Van hamme <nicefiction@gmail.com>
2021-08-23 12:18:06 +0200

    Completes Day 62.3 : Creating custom bindings in SwiftUI .

--- InstaFilter Techniques 2/Day 62/PropertyWrappers.swift
+++ InstaFilter Techniques 2/Day 62/PropertyWrappers.swift
@@ -12,6 +12,7 @@ struct PropertyWrappers: View {
    
    // MARK: - PROPERTY WRAPPERS
    
+   /// We still use @State private var to declare the blurAmount property :
    @State private var blurAmount: CGFloat = 0.00 {
       didSet {
          print("The bluramount has been changed to \(blurAmount).")
@@ -24,12 +25,26 @@ struct PropertyWrappers: View {
    
    var body: some View {
       
-      VStack {
+      let blur = Binding<CGFloat>(
+         get: { self.blurAmount },
+         set: {
+            self.blurAmount = $0
+            print("The new blur amount is \(blurAmount).")
+         }
+      )
+      
+      
+      return VStack {
          Text("Hello, World!")
             .font(.title)
+            /// We still use blur(radius: blurRadius) as the modifier for our text view :
             .blur(radius: blurAmount)
-         Slider(value: $blurAmount,
-                in: 0...10)
+         // Slider(value: $blurAmount, in: 0...10)
+         /// One thing that changed is the way we declare the binding in the slider:
+         /// rather than using $blurAmount we can just use blur.
+         /// This is because using the dollar sign is what gets us the two-way binding from some state,
+         /// but now that we’ve created the binding directly we no longer need it :
+         Slider(value: blur, in: 0...10)
             .padding()
       }
    }

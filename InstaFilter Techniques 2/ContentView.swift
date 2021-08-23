ee4b4f43ffca19d93856c7dd27c4dce5c0e091ff
Olivier Van hamme <nicefiction@gmail.com>
2021-08-23 11:28:51 +0200

    Initial Commit

---
+++ InstaFilter Techniques 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter Techniques 2
+//
+//  Created by Olivier Van hamme on 23/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaFilter Techniques 2/InstaFilter_Techniques_2App.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilter_Techniques_2App.swift
+//  InstaFilter Techniques 2
+//
+//  Created by Olivier Van hamme on 23/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilter_Techniques_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
